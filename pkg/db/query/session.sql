5cca8b0056e5c022665a88ecbfd39a82c00f3eab
paularah <p.arah@alustudent.com>
2022-11-01 14:54:54 +0200

    session query

---
+++ pkg/db/query/session.sql
@@ -0,0 +1,8 @@
+-- name: CreateSession :one
+INSERT INTO sessions (id, user, refresh_token, user_agent, ip, is_valid, expires_at) 
+VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *;
+
+-- name: GetSession :one 
+SELECT * FROM sessions
+WHERE id = $1 LIMIT 1;
+
