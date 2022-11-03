195bc4da95c89450218bdb381baf9906c5ef477d
paularah <p.arah@alustudent.com>
2022-11-03 07:55:47 +0200

    script env

--- .github/scripts/test.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash 
-echo "DB_DRIVER=postgres" >> app.env
-echo "DB_SOURCE=postgres://root:password@localhost:5432/wallet?sslmode=disable" >> app.env
-echo "SERVER_ADDRESS=0.0.0.0:8080" >> app.env
